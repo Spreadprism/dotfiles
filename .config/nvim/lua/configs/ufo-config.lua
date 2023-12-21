aa9c8ffed396fc0a53852f6d4da3777b4d9c4c51
Emeric Duguay <emericduguay@gmail.com>
2023-12-21 00:00:38 -0500

    changed ufo-fold look

--- .config/nvim/lua/configs/ufo-config.lua
+++ .config/nvim/lua/configs/ufo-config.lua
@@ -1,3 +1,30 @@
+local fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+	local newVirtText = {}
+	local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+	local sufWidth = vim.fn.strdisplaywidth(suffix)
+	local targetWidth = width - sufWidth
+	local curWidth = 0
+	for _, chunk in ipairs(virtText) do
+		local chunkText = chunk[1]
+		local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+		if targetWidth > curWidth + chunkWidth then
+			table.insert(newVirtText, chunk)
+		else
+			chunkText = truncate(chunkText, targetWidth - curWidth)
+			local hlGroup = chunk[2]
+			table.insert(newVirtText, { chunkText, hlGroup })
+			chunkWidth = vim.fn.strdisplaywidth(chunkText)
+			-- str width returned from truncate() may less than 2nd argument, need padding
+			if curWidth + chunkWidth < targetWidth then
+				suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+			end
+			break
+		end
+		curWidth = curWidth + chunkWidth
+	end
+	table.insert(newVirtText, { suffix, "MoreMsg" })
+	return newVirtText
+end
 return function()
 	vim.o.foldcolumn = "1" -- '0' is not bad
 	vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
@@ -7,15 +34,9 @@ return function()
 	vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 	vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-	-- BUG: this doesn't work (because async)
-	--
-	-- vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-	-- 	callback = function()
-	-- 		require("ufo").closeAllFolds()
-	-- 	end,
-	-- })
 	require("ufo").setup({
-		close_fold_kinds = { "function" },
+		close_fold_kinds = {},
+		fold_virt_text_handler = fold_virt_text_handler,
 		provider_selector = function(bufnr, filetype, buftype)
 			if filetype == "python" then
 				return { "treesitter", "indent" }
