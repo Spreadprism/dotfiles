151182e49d050bcd5081dcb29446c650d975be6e
Emeric Duguay <emericduguay@gmail.com>
2024-01-03 14:52:50 -0500

    formatting

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -46,7 +46,7 @@ return {
 				require("lazy").load({ plugins = { "dressing.nvim" } })
 				return vim.ui.select(...)
 			end
-      ---@diagnostic disable-next-line: duplicate-set-field
+			---@diagnostic disable-next-line: duplicate-set-field
 			vim.ui.input = function(...)
 				require("lazy").load({ plugins = { "dressing.nvim" } })
 				return vim.ui.input(...)
