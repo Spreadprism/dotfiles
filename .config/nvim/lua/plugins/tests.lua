52b24d187e283025ffc29fd0fa427e4963fe5d4e
Emeric Duguay <emericduguay@gmail.com>
2024-01-09 21:40:19 -0500

    added playerctl keybinds and window rules for GUFW

--- .config/nvim/lua/plugins/tests.lua
+++ .config/nvim/lua/plugins/tests.lua
@@ -12,7 +12,16 @@ return {
 			"rouge8/neotest-rust",
 		},
 		config = function()
-			local neotest_namespace = vim.api.nvim_create_namespace("neotest")
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
 			require("neotest").setup({
 				adapters = {
 					-- INFO: Golang
