7aeda57e61c6e23ba622faa5dfb38465a91fa317
Emeric Duguay <emericduguay@gmail.com>
2024-01-30 21:01:11 -0500

    Modified path and cpath to included rocks installed by nvim_rocks

--- .config/nvim/lua/init_lazy.lua
+++ .config/nvim/lua/init_lazy.lua
@@ -6,6 +6,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
+-- INFO: Add luarocks path for nvim_rocks
+local version = _VERSION:match("%d+%.%d+")
+package.path = package.path .. ";" .. vim.fn.stdpath("data") .. "/lazy/nvim_rocks/lib/lua/" .. version .. "/?.lua"
+package.cpath = package.cpath .. ";" .. vim.fn.stdpath("data") .. "/lazy/nvim_rocks/lib/lua/" .. version .. "/?.so"
+
 require("lazy").setup("plugins", {
 	ui = {
 		-- border = "rounded",
