f13531f4af4188e4e495f320f83141fa1795b9f1
Emeric Duguay <emericduguay@gmail.com>
2024-03-07 20:39:54 -0500

    added an icon picker :)

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -121,4 +121,10 @@ return {
 			auto_restore_enabled = false,
 		},
 	},
+	{
+		"ziontee113/icon-picker.nvim",
+		config = function()
+			require("icon-picker").setup({ disable_legacy_commands = true })
+		end,
+	},
 }
