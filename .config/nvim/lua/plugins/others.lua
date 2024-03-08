3f73be27a2db9f6a9d78f2436d122b09af98fec3
Emeric Duguay <emeric.duguay@gmail.com>
2024-03-08 13:59:20 -0500

    cleanup

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -1,17 +1,17 @@
 return {
-	{
-		-- INFO: luarocks
-		"theHamsta/nvim_rocks",
-		event = "VeryLazy",
-		priority = 100,
-		build = "pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
-		config = function()
-			local nvim_rocks = require("nvim_rocks")
-			nvim_rocks.ensure_installed("fzy")
-			nvim_rocks.ensure_installed("luafilesystem")
-			nvim_rocks.ensure_installed("magick")
-		end,
-	},
+	-- {
+ --    "thehamsta/nvim_rocks",
+ --    event = "verylazy",
+ --    priority = 100,
+ --    build = "pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
+ --    config = function()
+ --      local nvim_rocks = require("nvim_rocks")
+ --      nvim_rocks.ensure_installed("fzy")
+ --      nvim_rocks.ensure_installed("luafilesystem")
+ --      nvim_rocks.ensure_installed("magick")
+	-- 	-- INFO: luarocks
+	-- 	end,
+	-- },
 	{
 		"christoomey/vim-tmux-navigator",
 		event = "VeryLazy",
