952562b3b33e692f117407231688099346a73572
Emeric Duguay <emeric.duguay1@bell.ca>
2024-03-08 12:02:58 -0500

    ruff configs

--- .config/nvim/lua/configs/lsp/ruff_lsp.lua
+++ .config/nvim/lua/configs/lsp/ruff_lsp.lua
@@ -1,5 +1,8 @@
+-- ruff rules to ignore
+-- INFO: Most of these rules are not applicable because pyright already handles them
 local rules_to_ignore = {
 	"F401",
+	"F821",
 }
 
 return function(on_attach, capabilities)
